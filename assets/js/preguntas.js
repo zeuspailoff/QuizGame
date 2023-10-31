7180cb467b6aa945d474ba0be213acc5aba137c2
Abraham <228.abraham.diaz@gmail.com>
2023-10-31 18:58:12 +0000

    contenido de preguntas

--- assets/js/preguntas.js
+++ assets/js/preguntas.js
@@ -1,6 +1,20 @@
 let preguntas2 = true;
 let juegoTermino = true;
 let reiniciamosJuego = true;
+const pizza1 = "../recourses/icons/pizza/pizza-5.png";
+const pizza2 = "../recourses/icons/pizza/pizza-4.png";
+const pizza3 = "../recourses/icons/pizza/pizza-3.png";
+const pizza4 = "../recourses/icons/pizza/pizza-2.png";
+const pizza5 = "../recourses/icons/pizza/pizza-1.png";
+const pizza6 = "../recourses/icons/pizza/pizza.png";
+const ciencias = "../JSON/ciencias.json";
+const cultura = "../JSON/cultura-general.json";
+const deportes = "../JSON/deportes.json";
+const life_hacks = "../JSON/ciencias.json";
+const peliculas = "../JSON/movies.json";
+const musica = "../JSON/music.json";
+
+const pregunta = [ciencias,cultura, deportes, life_hacks, peliculas,  musica];
 
 class preguntas{
     
