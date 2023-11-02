45e4362912e61a0f21567eec070c348f9fe48f77
Abraham <228.abraham.diaz@gmail.com>
2023-11-02 17:16:27 +0000

    cambios en index y preguntas empezamos js

--- assets/js/preguntas.js
+++ assets/js/preguntas.js
@@ -1,43 +1,24 @@
-/*let preguntas2 = true;
-let juegoTermino = true;
-let reiniciamosJuego = true;
-const pizza1 = "../recourses/icons/pizza/pizza-5.png";
-const pizza2 = "../recourses/icons/pizza/pizza-4.png";
-const pizza3 = "../recourses/icons/pizza/pizza-3.png";
-const pizza4 = "../recourses/icons/pizza/pizza-2.png";
-const pizza5 = "../recourses/icons/pizza/pizza-1.png";
-const pizza6 = "../recourses/icons/pizza/pizza.png";
-const ciencias = "../JSON/ciencias.json";
-const cultura = "../JSON/cultura-general.json";
-const deportes = "../JSON/deportes.json";
-const life_hacks = "../JSON/ciencias.json";
-const peliculas = "../JSON/movies.json";
-const musica = "../JSON/music.json";
-
-const pregunta = [ciencias,cultura, deportes, life_hacks, peliculas,  musica];
-
-class preguntas{
-    
-}*/
-
-function escogerPreguntaRandom() {
-    escogerPregunta(Math.floor(Math.random() * Array.length))
+"use strict";
+// Seleciono con DOM la section donde crear la pregunta (<section id="pregunta"></section>)
+const jsonCategoriasURLs = [  
+    "../JSON/ciencias.json", 
+    "../JSON/cultura-general.json", 
+    "../JSON/deportes.json",
+    "../JSON/lifeHacks.json",
+    "../JSON/movies.json",
+    "../JSON/music.json"];
+const main = async () => {
+    const quiz = [];  
+    let puntos = 0;
+    for (let categoriaURL of jsonCategoriasURLs) {  
+          const resp = await fetch(categoriaURL);  
+           const preguntas = await resp.json();  
+           let indice = Math.floor(Math.random() * preguntas.length );
+           quiz.push(preguntas[indice])
+           console.log(quiz)
+            // genero num random entre 0 y preguntas.length-1
+             // añado la pregunta random preguntas[numRandom] a quiz (quiz.push)   preguntaElement.innerHTML(""); 
+             // creo la pregunta y respuestas con DOM }};
+    }
 }
-function escogerPregunta(n) {
-    let preguntas = readText("ciencias.json", "cultura-general.json", "deportes.json","life_hacks.json","movies.json","music.json") 
-    let interprete = JSON.parse(preguntas)
-    pregunta = interprete[n]
-    select_id("pregunta").innerHTML = pregunta.pregunta
-}
-
-function select_id(id) {
-    return document.getElementById(id)
-}
-
-function style(id) {
-    return select_id(id).style
-}
-
-function readText(ruta_Local) {
-    let texto = null
-} 
+main();
