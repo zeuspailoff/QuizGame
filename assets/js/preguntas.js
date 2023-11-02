8c6840b55d318c45857696e18a66fd15b59e4325
Mohammed El Mangadi <mohammedelmangadi@gmail.com>
2023-11-02 22:02:07 +0100

    responsive contenedor preguntas

--- assets/js/preguntas.js
+++ assets/js/preguntas.js
@@ -1,24 +1,25 @@
 "use strict";
 // Seleciono con DOM la section donde crear la pregunta (<section id="pregunta"></section>)
-const jsonCategoriasURLs = [  
-    "../JSON/ciencias.json", 
-    "../JSON/cultura-general.json", 
-    "../JSON/deportes.json",
-    "../JSON/lifeHacks.json",
-    "../JSON/movies.json",
-    "../JSON/music.json"];
+const jsonCategoriasURLs = [
+  "../JSON/ciencias.json",
+  "../JSON/cultura-general.json",
+  "../JSON/deportes.json",
+  "../JSON/lifeHacks.json",
+  "../JSON/movies.json",
+  "../JSON/music.json",
+];
 const main = async () => {
-    const quiz = [];  
-    let puntos = 0;
-    for (let categoriaURL of jsonCategoriasURLs) {  
-          const resp = await fetch(categoriaURL);  
-           const preguntas = await resp.json();  
-           let indice = Math.floor(Math.random() * preguntas.length );
-           quiz.push(preguntas[indice])
-           console.log(quiz)
-            // genero num random entre 0 y preguntas.length-1
-             // añado la pregunta random preguntas[numRandom] a quiz (quiz.push)   preguntaElement.innerHTML(""); 
-             // creo la pregunta y respuestas con DOM }};
-    }
-}
+  const quiz = [];
+  let puntos = 0;
+  for (let categoriaURL of jsonCategoriasURLs) {
+    const resp = await fetch(categoriaURL);
+    const preguntas = await resp.json();
+    let indice = Math.floor(Math.random() * preguntas.length);
+    quiz.push(preguntas[indice]);
+    console.log(preguntas, quiz.push(preguntas[indice]));
+    // genero num random entre 0 y preguntas.length-1
+    // añado la pregunta random preguntas[numRandom] a quiz (quiz.push)   preguntaElement.innerHTML("");
+    // creo la pregunta y respuestas con DOM }};
+  }
+};
 main();
