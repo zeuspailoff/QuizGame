b142a326ba5138636f6f94bac18e15fceabf1be3
Abraham <228.abraham.diaz@gmail.com>
2023-11-02 21:45:15 +0000

    segumos intentando js

--- assets/js/preguntas.js
+++ assets/js/preguntas.js
@@ -1,36 +1,61 @@
 "use strict";
-// Seleciono con DOM la section donde crear la pregunta (<section id="pregunta"></section>)
-const tituloPregunta = document.querySelector("#pregunta")
-const jsonCategoriasURLs = [  
-    "../JSON/ciencias.json", 
-    "../JSON/cultura-general.json", 
+
+const tituloPregunta = document.querySelector("#pregunta");
+const jsonCategoriasURLs = [
+    "../JSON/ciencias.json",
+    "../JSON/cultura-general.json",
     "../JSON/deportes.json",
     "../JSON/lifeHacks.json",
     "../JSON/movies.json",
-    "../JSON/music.json"];
-    const main = async () => {
-        const quiz = [];
-        let puntos = 0;
-      
-        for (let categoriaURL of jsonCategoriasURLs) {
-            
-            const resp = await fetch(categoriaURL);
-            const preguntas = await resp.json();
-            
-           
-            if (preguntas.preguntas && preguntas.preguntas.length > 0) {
-                let i = Math.floor(Math.random() * preguntas.preguntas.length);
-                quiz.push(preguntas.preguntas[i]);
-            }
-            console.log(quiz);
-        }
-        for (let pregunta of quiz) {
-            pregunta = document.createElement("div .preguntaPreguntas");
-            preguntaElement.innerHTML = pregunta.tituloPregunta;
+    "../JSON/music.json"
+];
 
-            document.getElementById("Pregunta").append(preguntaElement)
-            
+const main = async () => {
+    const quiz = [];
+    let puntos = 0;
+
+    for (let categoriaURL of jsonCategoriasURLs) {
+        const resp = await fetch(categoriaURL);
+        const preguntas = await resp.json();
+
+        if (preguntas.preguntas && preguntas.preguntas.length > 0) {
+            let i = Math.floor(Math.random() * preguntas.preguntas.length);
+            quiz.push(preguntas.preguntas[i]);
         }
-    };
+        console.log(quiz);
+    }
+
+    const preguntaElement = document.createElement("div");
+    preguntaElement.className = "preguntaPreguntas";
+    document.getElementById("pregunta").appendChild(preguntaElement);
+
+    const opcionesElement = document.createElement("div");
+    opcionesElement.className = "botonPregunta";
+    preguntaElement.appendChild(opcionesElement);
+    console.log(preguntaElement)
+
+    function mostrarPregunta(nombres) {
+        const pregunta = quiz[nombres];
+        preguntaElement.textContent = pregunta.tituloPregunta;
     
-    main();
+        opcionesElement.innerHTML = "";
+    
+        for (let i = 0; i < pregunta.opciones.length; i++) {
+            const opcionElement = document.createElement("div");
+            opcionElement.className = "opcionPregunta";
+            opcionElement.textContent = pregunta.opciones[i];
+    
+            const inputElement = document.createElement("input");
+            inputElement.type = "checkbox";
+            inputElement.name = "respuesta";
+    
+            opcionesElement.appendChild(opcionElement);
+            opcionesElement.appendChild(inputElement);
+            console.log(opcionElement)
+           
+        }
+    }
+    mostrarPregunta(i);
+};
+
+main();
