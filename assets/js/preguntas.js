0f09a866a8ed867a7e40578ba2cb28f5a705dbf5
Abraham <228.abraham.diaz@gmail.com>
2023-11-02 20:54:05 +0000

    añadimos js

--- assets/js/preguntas.js
+++ assets/js/preguntas.js
@@ -1,5 +1,6 @@
 "use strict";
 // Seleciono con DOM la section donde crear la pregunta (<section id="pregunta"></section>)
+const tituloPregunta = document.querySelector("#pregunta")
 const jsonCategoriasURLs = [  
     "../JSON/ciencias.json", 
     "../JSON/cultura-general.json", 
@@ -7,18 +8,29 @@ const jsonCategoriasURLs = [
     "../JSON/lifeHacks.json",
     "../JSON/movies.json",
     "../JSON/music.json"];
-const main = async () => {
-    const quiz = [];  
-    let puntos = 0;
-    for (let categoriaURL of jsonCategoriasURLs) {  
-          const resp = await fetch(categoriaURL);  
-           const preguntas = await resp.json();  
-           let indice = Math.floor(Math.random() * preguntas.length );
-           quiz.push(preguntas[indice])
-           console.log(quiz)
-            // genero num random entre 0 y preguntas.length-1
-             // añado la pregunta random preguntas[numRandom] a quiz (quiz.push)   preguntaElement.innerHTML(""); 
-             // creo la pregunta y respuestas con DOM }};
-    }
-}
-main();
+    const main = async () => {
+        const quiz = [];
+        let puntos = 0;
+      
+        for (let categoriaURL of jsonCategoriasURLs) {
+            
+            const resp = await fetch(categoriaURL);
+            const preguntas = await resp.json();
+            
+           
+            if (preguntas.preguntas && preguntas.preguntas.length > 0) {
+                let i = Math.floor(Math.random() * preguntas.preguntas.length);
+                quiz.push(preguntas.preguntas[i]);
+            }
+            console.log(quiz);
+        }
+        for (let pregunta of quiz) {
+            pregunta = document.createElement("div .preguntaPreguntas");
+            preguntaElement.innerHTML = pregunta.tituloPregunta;
+
+            document.getElementById("Pregunta").append(preguntaElement)
+            
+        }
+    };
+    
+    main();
